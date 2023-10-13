7e6c484fcc4cbf20129c8510b829ec8b809165b9
Rukkyaa <lbrt.axel@gmail.com>
2023-10-14 00:44:35 +0200

    feat(login): init the page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="text-[5rem]">
-        Hello world!
-      </h1>
+      <h1 className="text-[5rem]">Hello world!</h1>
+      <div>coucou</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/login/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <>
+      <h1 className="text-[5rem]">Login!</h1>
+    </>
+  );
+}
