1ed981296bbec07221efed0b6ba3ad07bb236515
Rukkyaa <lbrt.axel@gmail.com>
2023-10-14 01:00:02 +0200

    feat(tailwind): add brown colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          50: '#EDC4B3',
+          100: '#E6B8A2',
+          200: '#DEAB90',
+          300: '#D69F7E',
+          400: '#CD9777',
+          500: '#C38E70',
+          600: '#B07D62',
+          700: '#9D6B53',
+          800: '#8A5A44',
+          900: '#774936',
+        },
+      },
+    },
   },
   plugins: [],
 }
