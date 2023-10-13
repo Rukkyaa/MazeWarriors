33cb36767bffe77fc311d3b5d0085edb76550ec8
Rukkyaa <lbrt.axel@gmail.com>
2023-10-14 01:46:05 +0200

    feat(login): change the style of the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,35 @@
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <>
-      <NavLink replace to="/">
-        Home
-      </NavLink>
-      <NavLink replace to="/login">
-        Login
-      </NavLink>
-    </>
+    <div className="flex px-[5rem] bg-brown-700">
+      <div className="w-full flex justify-around gap-[2rem] py-[1.5rem]">
+        <NavbarItem to="/">Home</NavbarItem>
+        <NavbarItem to="/login">Login</NavbarItem>
+      </div>
+    </div>
+  );
+};
+
+interface NavbarItemProps {
+  to: string;
+  children: React.ReactNode;
+}
+
+const NavbarItem = ({ to, children }: NavbarItemProps) => {
+  return (
+    <NavLink
+      replace
+      to={to}
+      className={({ isActive }: { isActive: boolean }) =>
+        clsx("text-[1.5rem]", {
+          "text-white font-semibold": isActive,
+          "text-brown-900": !isActive,
+        })
+      }
+    >
+      {children}
+    </NavLink>
   );
 };
