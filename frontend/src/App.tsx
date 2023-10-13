7a4c4d3103f318449ae710e6c236ff022da3ae2e
Thomas Roulland <thomas.roulland28260@gmail.com>
2023-10-14 01:11:41 +0200

    feat(router): init the router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-[5rem]">Hello world!</h1>
-      <div>coucou</div>
+      <Navbar />
+      <Outlet />
     </>
   );
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,1 +1,14 @@
-export const Navbar = () => {};
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <>
+      <NavLink replace to="/">
+        Home
+      </NavLink>
+      <NavLink replace to="/login">
+        Login
+      </NavLink>
+    </>
+  );
+};
