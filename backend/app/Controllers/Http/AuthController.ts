0b8a1bd6d4ab7019063db4565f50f8205fe51eba
Thomas Roulland <thomas.roulland28260@gmail.com>
2023-10-16 00:28:40 +0200

    feat(logout) creation de la route du logout et de la méthode

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -4,7 +4,6 @@ import User from '../../Models/User'
 
 export default class AuthController {
   public async login({ request, response, auth }: HttpContextContract) {
-    console.log(auth.user)
 
     const loginSchema = schema.create({
       email: schema.string([rules.trim()]),
@@ -12,20 +11,39 @@ export default class AuthController {
     })
 
     const { email, password } = await request.validate({ schema: loginSchema })
-
+    
     const user = await User.findBy('email', email)
 
-    if (!user) {
-      return response.badRequest('Invalid credentials')
-    }
-
     try {
+      console.log(auth.isAuthenticated);
       await auth.use('web').verifyCredentials(email, password)
       await auth.login(user)
       const responseHeaders = response.getHeaders()
       console.log('Response Headers:', responseHeaders)
     } catch (error) {
-      return response.badRequest('Invalid credentials')
+      return response.badRequest('Invalid credentials 2 : '+ error)
+    }
+
+    return response.redirect('/')
+  }
+
+  public async logout({ request, response, auth }: HttpContextContract) {
+    const logoutSchema = schema.create({
+      email: schema.string([rules.trim()]),
+    })
+
+    const { email } = await request.validate({ schema: logoutSchema })
+    const loggedUser = auth.user
+
+    try {
+      if (loggedUser.email === email)
+        await auth.logout(loggedUser)
+      else
+        return response.badRequest('Invalid credentials')
+      const responseHeaders = response.getHeaders()
+      console.log('Response Headers:', responseHeaders)
+    } catch (error) {
+      return response.badRequest('Invalid credentials 2 : '+ error)
     }
 
     return response.redirect('/')
